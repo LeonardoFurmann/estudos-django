d910189c6e030153e6bef212fbf2b9ff232e368d
Leonardo Furmann <leohitnerf@gmail.com>
2024-07-07 15:48:35 -0300

    07/07 - estilos e deploy

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Produto
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     # print(dir(request))
     # print(f"user = {request.user}")
+
+    produtos = Produto.objects.all()
 
     if str(request.user) == 'AnonymousUser':
         teste ='Usuário não logado'
@@ -15,9 +21,29 @@ def index(request):
     context = {
         'curso': 'Curso',
         'logado': teste,
-        'nome': nome
+        'nome': nome,
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    #prod = Produto.objects.get(id=pk)
+    
+    prod = get_object_or_404(Produto, id=pk)
+
+    context = {
+        'produto' : prod
+    }
+    return render(request, 'produto.html', context)
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content= template.tender(), content_type = 'text/html; charset=utf8', status =404)
+
+def error500(request, exception):
+    template = loader.get_template('500.html')
+    return HttpResponse(content= template.tender(), content_type = 'text/html; charset=utf8', status =500)
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z=rh(#$^kr9-9c_33dylnn2-czc=kd+)v*-ziofvzob%0fim1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG true = modo desenvolvimento
+# DEBUG false = produção
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # usado durante o desenvolvimento
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')  #usado durante a produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
