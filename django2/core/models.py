ac29b95d3e478eb367fd22d5fdbe0f7e06c074b6
Leonardo Furmann <leohitnerf@gmail.com>
2024-07-09 22:52:43 -0300

    classe base

--- django2/core/models.py
+++ django2/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from stdimagem.models impoert StdImageField
 
-# Create your models here.
+#SIGNALS
+from django.db.models impoert signails
+from django.template.dafaultfilters import slugify
+
+class Base(models.models):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+
+    class Meta:
+        abstract = True
