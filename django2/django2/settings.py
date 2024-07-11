e6b199b7b1ac23d5ac5892110ef45082cdcb69a5
Leonardo Furmann <leohitnerf@gmail.com>
2024-07-10 22:37:22 -0300

    cadastrando produtos no banco

--- django2/django2/settings.py
+++ django2/django2/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = Path(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
